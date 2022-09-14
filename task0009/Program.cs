3b9a4cc7f3821505c89bc4b10b94a67da3177dad
Dmitrij <Dlevickijj17@yandex.ru>
2022-09-14 12:41:00 +0300

    yrok

---
+++ task0009/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(10, 100);
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10; //7
+    int secondDigit = num % 10; //8
+    if (firstDigit == secondDigit) return 0;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+int maxDigit = MaxDigit(number);
+string result = maxDigit > 0 ? maxDigit.ToString() : "Цифры равны";
+Console.WriteLine($"Наибольшая цифра числа {number} -> {result}");
